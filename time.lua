cdef5754344187a4de786f9116541fc224d5239c
Stefan Jacobs <stefan.jacobs@deutschebahn.com>
2020-01-06 13:45:07 +0100

    Added timetracking to hammerspoon

--- time.lua
+++ time.lua
@@ -5,5 +5,27 @@ function time()
     hs.alert.show("🕰️" .. os.date('%H:%M:%S', os.time()))
 end
 
-hs.hotkey.bind(hyper, "-", time)
-hs.hotkey.bind(hyperNoShift, "-", time)
+hs.hotkey.bind(hyper, "´", time)
+hs.hotkey.bind(hyperNoShift, "´", time)
+
+
+function timewarriorStatus()
+    local handle = io.popen("/usr/local/bin/timew summary")
+    local result = handle:read("*a")
+    handle:close()
+    hs.alert.show(result) -- TODO: Pretty format
+end
+
+hs.hotkey.bind(hyperNoShift, "-", timewarriorStatus)
+hs.hotkey.bind(hyper, "-", timewarriorStatus)
+
+
+function timewarriorToggle()
+    local handle = io.popen("~/bin/tw.sh")
+    local result = handle:read("*a")
+    handle:close()
+    hs.alert.show(result) -- TODO: Pretty format
+end
+
+hs.hotkey.bind(hyperNoShift, "+", timewarriorToggle)
+hs.hotkey.bind(hyper, "+", timewarriorToggle)
