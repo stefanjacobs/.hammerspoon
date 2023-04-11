e4c6879f789b883a61d289716729e6f9e46ea889
Stefan Jacobs <stefan.jacobs@deutschebahn.com>
2023-04-11 11:28:34 +0200

    Added help for the app-shortcuts

--- apps.lua
+++ apps.lua
@@ -13,7 +13,8 @@ local applicationHotkeys = {
     e = 'Microsoft Excel',
     p = 'Preview',
     t = 'Microsoft Teams',
-    x = 'KeePassXC'
+    x = 'KeePassXC',
+    u = 'Obsidian'
 }
 
 -- Register Hyper + Key to Launch or Focus the given app
@@ -29,3 +30,28 @@ for key, app in pairs(applicationHotkeys) do
 end
 
 -- hs.alert.show('Apps 📌: Ready to rock 🤘!')
+
+local function pairsByKeys (t, f)
+    local a = {}
+    for n in pairs(t) do table.insert(a, n) end
+    table.sort(a, f)
+    local i = 0      -- iterator variable
+    local iter = function ()   -- iterator function
+        i = i + 1
+        if a[i] == nil then return nil
+        else return a[i], t[a[i]]
+        end
+    end
+    return iter
+end
+
+function appHelp()
+    msg = ""
+    for key, app in pairsByKeys(applicationHotkeys) do
+        msg = msg .. key .. ":\t" .. app .. "\n"
+    end
+    hs.alert.show(msg, 5)
+end
+
+hs.hotkey.bind(hyper, "ß", appHelp)
+hs.hotkey.bind(hyperNoShift, "ß", appHelp)
